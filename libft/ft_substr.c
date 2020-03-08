834c7057efdc80515b2bce59ce6f4fbfd7c4126b
mhaman <mhaman@student.le-101.fr>
2020-03-08 20:56:44 +0100

    fonctionnement get_next_line

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,21 +6,11 @@
 /*   By: mhaman <mhaman@student.le-101.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/11 19:06:39 by mhaman            #+#    #+#             */
-/*   Updated: 2020/03/04 13:27:21 by mhaman           ###   ########lyon.fr   */
+/*   Updated: 2020/03/08 17:35:48 by mhaman           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static size_t	ft_strnlen(const char *s, unsigned int len)
-{
-	size_t i;
-
-	i = 0;
-	while (len > i && s[i])
-		i++;
-	return (i);
-}
 
 extern	char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -30,8 +20,6 @@ extern	char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (s == NULL)
 		return (NULL);
 	i = 0;
-	if (start > ft_strnlen(s, start) && *s)
-		return (ft_strdup(""));
 	if (!(s2 = ft_calloc((len + 1), sizeof(char))))
 		return (0);
 	while (len > i && s[start])
--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: mhaman <mhaman@student.le-101.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 09:53:46 by mhaman            #+#    #+#             */
-/*   Updated: 2020/03/06 15:31:58 by mhaman           ###   ########lyon.fr   */
+/*   Updated: 2020/03/08 20:29:51 by mhaman           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <string.h>
 #include "libft/libft.h"
 
-enum			e_text 
+enum			e_text
 {
 	NORTH,
 	SOUTH,
@@ -58,33 +58,50 @@ int parse_file_cub(t_cub map, char **argv, int argc)
 	int fd;
 	char *line;
 	check_file_validity(map,argv, argc);
-
+	check_instruction_validity(map,argv);
 return(0);
+}
+
+int	check_instruction_validity(t_cub map, char ** argv)
+{
+	int fd;
+	char *line;
+
+	line = NULL;
+	fd = open(argv[1], O_RDONLY);
+	while (get_next_line(fd, &line) != 0)
+	{
+		if (line[0] != 0)
+		{
+
+		}
+	}
 }
 int	check_file_validity(t_cub map,char **argv,int argc)
 {
 	int fd;
 	char *file;
+
+	if (argc < 2)
+		error_str_return("Missing map file");
+	if(argc == 3)
+		if (ft_strncmp(argv[2], "--save", 8) != 0)
+			error_str_return("Bad option here only --save available");
 	if ((file = ft_strnstr(argv[1], ".cub", ft_strlen(argv[1]))) == NULL)
-	{
-		dprintf(1,"ERROR\nNo .cub format found");
-		exit(1);
-	}
+		error_str_return("No .cub format found");
 	else
 	{
-		file += 4;
+		file -= 1;
+		if (*file == 0)
+			error_str_return("File need name");
+		file += 5;
 		if (*file != 0)
-		{
-			printf("[%s]\n",file);
-			puts("ERROR\nBad format file");
-		}
+			error_str_return("Bad Format File");
 	}
 	if ((fd = open(argv[1],O_RDONLY)) == -1)
-		dprintf(1,"ERROR\nCould not open file");
+		error_str_return("Could not open file");
 	else
-	{
 		close(fd);
-	}
 	return (1);
 }
 
@@ -96,11 +113,13 @@ int error_str_return(char *str)
 
 int main(int argc, char** argv)
 {
-	char *line;     
+	char *line;
 	char	buff[1024];
-	
+
 	t_cub *map;
 	ft_bzero((map = malloc(sizeof(t_cub))), sizeof(t_cub));
 	parse_file_cub(*map, argv, argc);
+	printf("file valid\n");
+	free(map);
 	return (0);
 }
