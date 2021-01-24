8d45873d0adfbd0354f8518d806bfd8cdac9fc4d
mhaman <mhaman@student.le-101.fr>
2021-01-24 10:51:48 +0100

    test norme

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: mhaman <mhaman@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/16 11:57:10 by mhaman            #+#    #+#             */
-/*   Updated: 2021/01/23 10:53:49 by mhaman           ###   ########lyon.fr   */
+/*   Updated: 2021/01/24 10:51:34 by mhaman           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@
 # define MVS 0.05
 # define RTS 0.10
 
-enum				e_text
+enum					e_text
 {
 	NO,
 	SO,
@@ -37,132 +37,131 @@ enum				e_text
 	TEXTURE_COUNT
 };
 
-typedef struct			s_bpm
+typedef	struct			s_bpm
 {
 	unsigned char	bitmap_type[2];
-	int		file_size;
-	short		reserved1;
-	short		reserved2;
+	int				file_size;
+	short			reserved1;
+	short			reserved2;
 	unsigned int	offset_bits;
-}				t_bpm;
+}						t_bpm;
 
-typedef struct			s_bpm2
+typedef	struct			s_bpm2
 {
 	unsigned int	size_header;
 	unsigned int	width;
 	unsigned int	height;
-	short int	planes;
-	short int	bit_count;
+	short int		planes;
+	short int		bit_count;
 	unsigned int	compression;
 	unsigned int	image_size;
 	unsigned int	ppm_x;
 	unsigned int	ppm_y;
 	unsigned int	clr_used;
 	unsigned int	clr_important;
-}				t_bpm2;
+}						t_bpm2;
 
-typedef struct			s_mlx
+typedef	struct			s_mlx
 {
-	void		*ptr;
-	void		*win;
-	int		*data;
-	int		*data_text[TEXTURE_COUNT];
-	void		*img;
-	void		*img_txt[TEXTURE_COUNT];
-	int		bpp;
-	int		line_size;
-	int		endien;
-	int		h[TEXTURE_COUNT];
-	int		w[TEXTURE_COUNT];
-
-}				t_mlx;
+	void			*ptr;
+	void			*win;
+	int				*data;
+	int				*data_text[TEXTURE_COUNT];
+	void			*img;
+	void			*img_txt[TEXTURE_COUNT];
+	int				bpp;
+	int				line_size;
+	int				endien;
+	int				h[TEXTURE_COUNT];
+	int				w[TEXTURE_COUNT];
+}						t_mlx;
 
 typedef	struct			s_float
 {
-	double		x;
-	double		y;
-}				t_float;
+	double			x;
+	double			y;
+}						t_float;
 typedef	struct			s_int
 {
-	int		x;
-	int		y;
-}				t_int;
+	int				x;
+	int				y;
+}						t_int;
 
 typedef	struct			s_ste
 {
-	int		start;
-	int		end;
-}				t_ste;
+	int				start;
+	int				end;
+}						t_ste;
 
-typedef struct			s_player
+typedef	struct			s_player
 {
-	t_float		pos;
-	char		orientation;
-	t_float		dir;
-	t_float		plane;
-	t_float		camera;
-}				t_player;
+	t_float			pos;
+	char			orientation;
+	t_float			dir;
+	t_float			plane;
+	t_float			camera;
+}					t_player;
 
-typedef struct			s_move
+typedef	struct			s_move
 {
-	int		forward;
-	int		backward;
-	int		rightside;
-	int		leftside;
-	int		rightrot;
-	int		leftrot;
-	int		close;
-}				t_move;
+	int				forward;
+	int				backward;
+	int				rightside;
+	int				leftside;
+	int				rightrot;
+	int				leftrot;
+	int				close;
+}						t_move;
 
-typedef struct			s_sprite
+typedef	struct			s_sprite
 {
-	t_float		pos;
-	double		dist;
-}				t_sprite;
+	t_float			pos;
+	double			dist;
+}						t_sprite;
 
-typedef struct			s_ray
+typedef	struct			s_ray
 {
-	t_float		dir;
-	t_int		pos;
-	t_float		sdist;
-	t_float		ddist;
-	double		perpdist;
-	t_int		step;
-	int		height;
-}				t_ray;
+	t_float			dir;
+	t_int			pos;
+	t_float			sdist;
+	t_float			ddist;
+	double			perpdist;
+	t_int			step;
+	int				height;
+}						t_ray;
 
-typedef struct			s_cub
+typedef	struct			s_cub
 {
-	t_int		screen;
-	int		colorsky;
-	int		colorfloor;
-	int		texture[TEXTURE_COUNT];
-	char		*text[TEXTURE_COUNT];
-	char		**map;
-	int		*screenpx;
-	int		hit;
-	int		side;
-	int		textpx;
-	double		textstep;
-	t_int		tex;
-	double		texpos;
-	t_ste		draw;
-	t_int		mapsize;
-	t_mlx		mlx;
-	t_ray		ray;
-	t_player	pla;
-	int		spriteheight;
-	t_ste		sprite_y;
-	t_ste		sprite_x;
-	int		nbsprite;
-	t_sprite	*sprite;
-	t_move		move;
-	double		zbuffer[1920];
-	int		color;
-	t_float		spr;
-	t_float		trans;
-	int		screenshot;
-}				t_cub;
+	t_int			screen;
+	int				colorsky;
+	int				colorfloor;
+	int				texture[TEXTURE_COUNT];
+	char			*text[TEXTURE_COUNT];
+	char			**map;
+	int				*screenpx;
+	int				hit;
+	int				side;
+	int				textpx;
+	double			textstep;
+	t_int			tex;
+	double			texpos;
+	t_ste			draw;
+	t_int			mapsize;
+	t_mlx			mlx;
+	t_ray			ray;
+	t_player		pla;
+	int				spriteheight;
+	t_ste			sprite_y;
+	t_ste			sprite_x;
+	int				nbsprite;
+	t_sprite		*sprite;
+	t_move			move;
+	double			zbuffer[1920];
+	int				color;
+	t_float			spr;
+	t_float			trans;
+	int				screenshot;
+}						t_cub;
 
 void	parse_file_cub(t_cub *map, char **argv, int argc);
 int	check_instruction_validity(t_cub *map, char **argv);
